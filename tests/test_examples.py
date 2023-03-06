f34c5663c1eed28f4c24681c865f22a9073bd58e
Louis <louismartin806@gmail.com>
2023-03-06 13:50:36 +0000

    errors & testing bites complete

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,7 +2,7 @@ from lib.add_five import *
 from lib.greet import *
 from lib.check_codeword import *
 from lib.report_length import *
-from lib.reminder import *
+# from lib.reminder import *   This has moved down to the reminder test to highlight new functionality
 from lib.counter import *
 from lib.string_builder import *
 from lib.gratitudes import *
@@ -27,12 +27,22 @@ def test_report_length_returns_three_for_ape():
     result = report_length('Ape')
     assert result == "This string was 3 characters long."
 
+import pytest # <-- New code
+from lib.reminder import *
+
+def test_reminder():
+    reminder = Reminder("Kay")
+    with pytest.raises(Exception) as e: # <-- New code
+        reminder.remind()
+    error_message = str(e.value) # <-- New code
+    assert error_message == "No reminder set!"
+
 def test_reminds_the_user_to_do_a_task():
     reminder = Reminder("Kay")
     reminder.remind_me_to("Walk the dog")
     result = reminder.remind()
     assert result == "Walk the dog, Kay!"
-
+    
 def test_counter_counts_to_num():
     count = Counter()
     count.add(5)
@@ -56,3 +66,32 @@ def test_gratitudes():
     gratitudes.add('nice eyes')
     result = gratitudes.format()
     assert result == "Be grateful for: thick thighs, nice eyes"
+
+import pytest
+from lib.present import *
+
+def test_present():
+    present = Present()
+    present.wrap("Toy")
+    with pytest.raises(Exception) as e:
+        present.wrap("Toy")
+    error_message = str(e.value)
+    assert error_message == "A contents has already been wrapped."
+    result = present.unwrap()
+    assert result == "Toy"
+    nopresent = Present()
+    with pytest.raises(Exception) as e:
+        nopresent.unwrap()
+    error_message2 = str(e.value)
+    assert error_message2 == "No contents have been wrapped."
+
+from lib.password_checker import *
+
+def test_password_checker():
+    pass_checker = PasswordChecker()
+    result = pass_checker.check("12345678")
+    assert result == True
+    with pytest.raises(Exception) as e:
+        pass_checker.check("nope")
+    error_message = str(e.value)
+    assert error_message == "Invalid password, must be 8+ characters."
